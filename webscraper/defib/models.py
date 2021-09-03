1e4c2d0526fe8a09cda32d42ead820b5a21db20e
Ba12ley <baiz@me.com>
2021-09-03 04:46:51 +0100

    model and dummy tasks added

--- webscraper/defib/models.py
+++ webscraper/defib/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class DefibLocationNI(models.Model):
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False,)
+    location_name = models.CharField(max_length=240)
+    location_lat = models.CharField(max_length=240)
+    location_long = models.CharField(max_length=240)
+    date_added = models.DateField(auto_created=True)
+    date_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.location_name
--- webscraper/defib/tasks.py
+++ webscraper/defib/tasks.py
@@ -1,2 +1,9 @@
 from celery import shared_task, task
 
+@shared_task()
+def do_something_async(unscheduled):
+    print(unscheduled)
+
+@task
+def do_something_scheduled():
+    print("Scheduled task")
